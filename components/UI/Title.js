36ace41202537e8e9afbb012ece9bbd58889c1d8
Heena Varu <heena.nvr@gmail.com>
2024-08-16 14:10:43 +0200

    Added platform specific code and files and also added status bar style

--- components/UI/Title.js
+++ components/UI/Title.js
@@ -1,4 +1,4 @@
-import { Text, StyleSheet, Dimensions } from "react-native";
+import { Text, StyleSheet, Dimensions, Platform } from "react-native";
 import Colors from "../../Constants/Colors";
 
 function Title({ children }) {
@@ -7,18 +7,19 @@ function Title({ children }) {
 
 export default Title;
 
-const deviceWidth = Dimensions.get('window').width;
+const deviceWidth = Dimensions.get("window").width;
 
 const styles = StyleSheet.create({
   title: {
-    fontFamily: 'open-sans-bold',
+    fontFamily: "open-sans-bold",
     fontSize: deviceWidth < 380 ? 24 : 28,
     color: Colors.blue,
     textAlign: "center",
-    // borderWidth: 2,
-    // borderColor: Colors.secondary,
+    // borderWidth: Platform.OS === 'android' ? 2 : 0,
+    borderWidth: Platform.select({iOS: 0, android: 2}),
+    borderColor: Colors.secondary,
     padding: 12,
-    maxWidth: '80%',
-    width: 300
+    maxWidth: "80%",
+    width: 300, 
   },
 });
